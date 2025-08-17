4c5d5a3a1be449a828dfd60e557b68454426b95a
basleunissen72-netizen <basleunissen72@gmail.com>
2025-08-17 10:59:11 +0200

    Update telegram-helper.ts

--- src/telegram-helper.ts
+++ src/telegram-helper.ts
@@ -4,6 +4,6 @@ if (!process.env.BOT_TOKEN) {
   throw new Error('BOT_TOKEN ontbreekt in env')
 }
 
-export const botLink = 'IsHetEenPoesofeenBot' // vrij label in je DB-scope
+export const botLink = 'IsHetEenPoesofeenBot' // vrij label voor jouw DB-scope
 export const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: false })
 // Geen polling; we gebruiken webhook + Express route + bot.processUpdate
